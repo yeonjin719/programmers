77e2543eb6591e84df58c691cc3608365258ef09
김연진 <111187984+yeonjin719@users.noreply.github.com>
2024-01-20 17:20:10 +0900

    Create 같은숫자는싫어.py

---
+++ 같은숫자는싫어.py
@@ -0,0 +1,11 @@
+def solution(arr):
+    answer = []
+    for i in range(len(arr)):
+        if i == 0:
+            answer.append(arr[i])
+        else: 
+            if answer[-1] == arr[i]:
+                pass
+            else:
+                answer.append(arr[i])
+    return answer
